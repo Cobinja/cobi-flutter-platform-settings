a9d748fd26d410296c1feee3d2007b02ff94b8ee
Cobinja <cobinja@yahoo.de>
2022-01-27 14:20:45 +0100

    Remove some unnecessary imports

--- lib/src/settings_screen.dart
+++ lib/src/settings_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cobi_flutter_platform_settings/src/settings_widget_base.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 /// The uppermost container for platform settings widgets
